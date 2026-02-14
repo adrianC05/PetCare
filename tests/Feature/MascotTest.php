67a0717a5b48f62cc80e157e4a52d99eee85dc38
Stephy3 <131786609+Stephy3@users.noreply.github.com>
2026-02-13 20:04:40 -0500

    fix: eliminar usos duplicados de TestCase en Pest

--- tests/Feature/MascotTest.php
+++ tests/Feature/MascotTest.php
@@ -2,23 +2,17 @@
 
 use App\Models\User;
 use App\Models\Mascot;
-use Tests\TestCase;
 use function Pest\Laravel\actingAs;
-use function Pest\Laravel\get;
 use function Pest\Laravel\assertDatabaseHas;
 
-uses(TestCase::class);
-
-// 1. CAMBIAMOS ESTA PRUEBA
 test('un usuario autenticado puede acceder al panel principal', function () {
     $user = User::factory()->create();
 
     actingAs($user)
-        ->get('/admin') // <--- Cambiamos de /admin/mascots a /admin
+        ->get('/admin')
         ->assertStatus(200);
 });
 
-// 2. ESTA PRUEBA SE QUEDA EXACTAMENTE IGUAL (¡Esta ya pasó en GitHub!)
 test('se puede crear una mascota en la base de datos', function () {
     $owner = User::factory()->create();
 
